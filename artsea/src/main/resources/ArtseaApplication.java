ae1063cb759311684276ea9f1f4b5dccef0afe8d
hindochahitarth <hindochahitarth@gmail.com>
2025-07-31 13:33:54 +0530

    Changes

--- artsea/src/main/resources/ArtseaApplication.java
+++ artsea/src/main/resources/ArtseaApplication.java
@@ -2,9 +2,13 @@ package com.art.artsea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.art.artsea")
+@EntityScan(basePackages = "com.art.artsea")
 public class ArtseaApplication {
 
 	public static void main(String[] args) {
